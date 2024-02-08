fbef1c124cecf2b4120fc09c6ec9c1ebdcd2fe3b
ankush4gupta <87088148+ankush4gupta@users.noreply.github.com>
2024-02-08 22:21:48 +0530

    pushing dashboard application code

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { abel } from '@/app/ui/fonts'
+export default function Page() {
+    return (
+        <div>
+            <h1> Dashboard Page</h1>
+            <p className={`${abel.className}`}>This is page content.</p>
+        </div>
+    )
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana, Abel } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] })
+export const abel = Abel({ weight: ['400'], subsets: ['latin'] })
+// export const lusitana = Lusitana({
+//     weight: ,
+//     subsets: ['latin'],
+// });
